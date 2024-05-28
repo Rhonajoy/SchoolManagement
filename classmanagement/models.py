d73b74cb57fbd101eec70e53fd84ae698b29a9d5
RhonaJoy <rhonajoy8@gmail.com>
2024-05-28 10:16:37 +0300

    add models

--- classmanagement/models.py
+++ classmanagement/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+    name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class ClassStream(models.Model):
+    name=models.CharField(max_length=10)
+    class_id=models.ForeignKey(Class,related_name='streams',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    class_stream=models.ForeignKey(ClassStream,related_name='students',on_delete=models.CASCADE)   
+    
+    def __str__(self):
+        return self.name 
+        
